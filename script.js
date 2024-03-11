8a4a09042b8c0f257e728285bfc860fb1475145d
OfficialNeil <neil7221@gmail.com>
2024-03-11 11:31:40 -0400

    On branch main  Changes to be committed: 	new file:   img/homepfp.JPG 	modified:   index.css 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -21,3 +21,49 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+const phrases = ['I\'m a Student', 'I\'m an Engineer', 'I\'m a Problem-Solver'];
+let index = 0;
+let letterIndex = 0;
+let direction = 1;
+
+function updateText() {
+  const textElement = document.getElementById('text');
+  const cursorElement = document.getElementById('cursor');
+  
+  textElement.textContent = phrases[index].substring(0, letterIndex + 1);
+  
+  if (letterIndex === phrases[index].length - 1) {
+    direction = -1;
+  } else if (letterIndex === 0 && direction === -1) {
+    direction = 1;
+    index = (index + 1) % phrases.length;
+  }
+  
+  letterIndex += direction;
+  
+  cursorElement.style.visibility = (direction === 1 && letterIndex < phrases[index].length) ? 'visible' : 'hidden';
+}
+
+function startAnimation() {
+  setInterval(updateText, 250);
+}
+
+startAnimation();
+
+document.addEventListener('DOMContentLoaded', function() {
+    let animatedContent = document.querySelector('.animated-content');
+    let animatedImage = document.querySelector('.animated-image');
+    let animatedText = document.querySelector('.animated-text');
+    
+    animatedContent.classList.add('animate');
+    animatedImage.classList.add('animate');
+    animatedText.classList.add('animate');
+  });
+
+  
+  
+  
+  
+  
