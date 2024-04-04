ed39d9c90c9522ba28f7ecd4a4d46e112548e624
OfficialNeil <neil7221@gmail.com>
2024-04-04 13:45:35 -0400

    On branch main  Changes to be committed: 	modified:   contact.php 	modified:   index.css 	modified:   index.html

--- contact.php
+++ contact.php
@@ -1,59 +1,36 @@
-<?php 
-// Email configuration 
-$toEmail = 'neil7221@gmail.com'; 
-$fromName = 'Sender Name'; 
-$formEmail = 'sender@example.com'; 
- 
-$postData = $statusMsg = $valErr = ''; 
-$status = 'error'; 
- 
-// If the form is submitted 
-if(isset($_POST['submit'])){ 
-    // Get the submitted form data 
-    $postData = $_POST; 
-    $name = trim($_POST['name']); 
-    $email = trim($_POST['email']); 
-    $subject = trim($_POST['subject']); 
-    $message = trim($_POST['message']); 
-     
-    // Validate form fields 
-    if(empty($name)){ 
-         $valErr .= 'Please enter your name.<br/>'; 
-    } 
-    if(empty($email) || filter_var($email, FILTER_VALIDATE_EMAIL) === false){ 
-        $valErr .= 'Please enter a valid email.<br/>'; 
-    } 
-    if(empty($subject)){ 
-        $valErr .= 'Please enter subject.<br/>'; 
-    } 
-    if(empty($message)){ 
-        $valErr .= 'Please enter your message.<br/>'; 
-    } 
-     
-    if(empty($valErr)){ 
-        // Send email notification to the site admin 
-        $subject = 'New contact request submitted'; 
-        $htmlContent = " 
-            <h2>Contact Request Details</h2> 
-            <p><b>Name: </b>".$name."</p> 
-            <p><b>Email: </b>".$email."</p> 
-            <p><b>Subject: </b>".$subject."</p> 
-            <p><b>Message: </b>".$message."</p> 
-        "; 
-         
-        // Always set content-type when sending HTML email 
-        $headers = "MIME-Version: 1.0" . "\r\n"; 
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
-        // Header for sender info 
-        $headers .= 'From:'.$fromName.' <'.$formEmail.'>' . "\r\n"; 
-         
-        // Send email 
-        @mail($toEmail, $subject, $htmlContent, $headers); 
-         
-        $status = 'success'; 
-        $statusMsg = 'Thank you! Your contact request has submitted successfully, we will get back to you soon.'; 
-        $postData = ''; 
-    }else{ 
-        $statusMsg = '<p>Please fill all the mandatory fields:</p>'.trim($valErr, '<br/>'); 
-    } 
+<?php
+// Output messages
+$responses = [];
+// Check if the form was submitted
+if (isset($_POST['email'], $_POST['subject'], $_POST['name'], $_POST['msg'])) {
+	// Validate email adress
+	if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$responses[] = 'Email is not valid!';
+	}
+	// Make sure the form fields are not empty
+	if (empty($_POST['email']) || empty($_POST['subject']) || empty($_POST['name']) || empty($_POST['msg'])) {
+		$responses[] = 'Please complete all fields!';
+	} 
+	// If there are no errors
+	if (!$responses) {
+		// Where to send the mail? It should be your email address
+		$to      = 'neil7221@gmail.com';
+		// Send mail from which email address?
+		$from = 'neil7221alt@gmail.com';
+		// Mail subject
+		$subject = $_POST['subject'];
+		// Mail message
+		$message = $_POST['msg'];
+		// Mail headers
+		$headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $_POST['email'] . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+		// Try to send the mail
+		if (mail($to, $subject, $message, $headers)) {
+			// Success
+			$responses[] = 'Message sent!';		
+		} else {
+			// Fail
+			$responses[] = 'Message could not be sent! Please check your mail server settings!';
+		}
+	}
 }
+?>
